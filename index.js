4c2851b00494050052cfbf605b2f4638e184ab3b
simralgasimov <simralgasimov@gmail.com>
2023-10-11 22:41:34 +0400

    js

---
+++ index.js
@@ -0,0 +1,45 @@
+let a=12345
+let b=9876
+console.log(a*b);
+
+let c='121919220'
+console.log(c[6]);
+
+
+let d="2"
+console.log(d+1+0);
+// soru
+
+// sadece toplamada kecerli yan yana gelmesi
+let f="2" 
+console.log(d-1+0);
+
+// true 1 false 0
+let g=true   
+let h=false
+console.log(g+h);
+
+
+let j=6
+let k="3"
+console.log(j/k);
+
+let e=4
+let r=5
+let t="px"
+console.log(e+r+t);
+
+// null bos deyer
+let o=null
+let p=1
+console.log(o+p);
+
+
+let m=undefined
+let n=1
+console.log(m+n);
+
+
+let z="4px"
+let l=2
+console.log(z-l);
